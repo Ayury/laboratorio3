5cae347b28a695063039b28e85fd7c3ec7289058
Ayury <yury.agrazal@utp.ac.pa>
2021-11-16 11:46:55 -0500

    final

--- config/conexion.php
+++ config/conexion.php
@@ -8,7 +8,7 @@
       $conn = new PDO("mysql:host=".HOST_NAME.";dbname=".DATABASE, USER_NAME, PASS);
       // set the PDO error mode to exception
       $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-      echo " e <br>";
+      echo "  <br>";
     } catch(PDOException $e) {
       echo "Problema de conexión : " . $e->getMessage();
     }
